b5a140aeadc5b5b84bd3cd7d69b64d9624cc2823
Luis Correa <henriqueluis785@gmail.com>
2023-05-08 04:49:31 -0300

    controller and models

--- src/api.js
+++ src/api.js
@@ -9,12 +9,18 @@ app.use('/', router.get('/',(req, res, next)=>{
     res.status(200).send("<h1>API - CHAT</h1>")
 }));
 
-app.use('/', router.get('/sobre',(req, res, next)=>{
+app.use('/sobre', router.get('/sobre',(req, res, next)=>{
     res.status(200).send({
         "nome":"API - CHAT",
         "versão":"0.1.0",
         "autor": "Luis Corrêa"
     })
+}));
+
+app.use('/salas', router.get('/salas',(req, res, next)=>{
+    const salaController = require("./controllers/salaController");
+    let resp = salaController.get();
+    res.status(200).send(resp);
 }));
 
 module.exports=app;
